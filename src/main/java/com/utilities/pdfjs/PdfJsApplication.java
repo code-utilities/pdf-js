74b7d681f11dae3cb0f91487ea4190478c8e323f
Lindo Manana <mananalm@telkom.co.za>
2020-05-15 15:11:02 +0200

    inital commit

---
+++ src/main/java/com/utilities/pdfjs/PdfJsApplication.java
@@ -0,0 +1,13 @@
+package com.utilities.pdfjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfJsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utilities/pdfjs/PdfJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utilities.pdfjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
